4e753c0a9e719820e5db112045e048766e5025a0
L <lpkiki1229@gmail.com>
2018-01-24 14:40:56 +0800

    Initial Commit

---
+++ TimeManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimeManager
+//
+//  Created by Lpkiki on 2018/1/24.
+//  Copyright © 2018年 kiki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TimeManager/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TimeManager
+//
+//  Created by Lpkiki on 2018/1/24.
+//  Copyright © 2018年 kiki. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
